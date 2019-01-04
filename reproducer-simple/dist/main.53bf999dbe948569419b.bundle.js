dece0645620f8b6eabe4a91af318f5936a68d7c0
Erica <erica.gucciardo@updater.com>
2019-01-04 16:15:54 -0500

    remove connectedCallback.super

--- reproducer-simple/dist/main.53bf999dbe948569419b.bundle.js
+++ reproducer-simple/dist/main.53bf999dbe948569419b.bundle.js
@@ -72,8 +72,6 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -111,7 +109,6 @@ var Test = function (_CustomElement2) {
   }, {
     key: 'connectedCallback',
     value: function connectedCallback() {
-      _get(Test.prototype.__proto__ || Object.getPrototypeOf(Test.prototype), 'connectedCallback', this) && _get(Test.prototype.__proto__ || Object.getPrototypeOf(Test.prototype), 'connectedCallback', this).call(this);
       this.innerHTML = '\n    <button>Reproduce</button>\n    ';
       this.querySelector('button').addEventListener('click', this.fireChangeEvent);
     }
@@ -157,4 +154,4 @@ document.querySelector('#gCrWeb').addEventListener('click', function () {
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=main.74a5aeb9c7f4d5c85d44.bundle.js.map
+//# sourceMappingURL=main.53bf999dbe948569419b.bundle.js.map
--- reproducer-simple/src/index.js
+++ reproducer-simple/src/index.js
@@ -13,7 +13,6 @@ class Test extends HTMLElement {
   }
 
   connectedCallback() {
-    super.connectedCallback && super.connectedCallback();
     this.innerHTML = `
     <button>Reproduce</button>
     `;

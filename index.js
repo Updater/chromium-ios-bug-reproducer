766822d60b09174a9f457508ca09c2ed94cb9eba
Erica <erica.gucciardo@updater.com>
2019-01-03 23:09:53 -0500

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const logToConsole = (html) => {
+  const initalHtml = document.querySelector('#console').innerHTML;
+  document.querySelector('#console').innerHTML = html + initalHtml;
+}
+
+document.querySelector('#button').addEventListener('click', () => {
+  document.querySelector("#a").value = {
+    x: () => {}
+  };
+
+  document.querySelector("#a").dispatchEvent(new Event('change'));
+
+  try {
+    console.log('a')
+  } catch (ex) {
+    logToConsole(`<code><pre>ConsoleLog Exception: ${ex.message}\n${ex.stack}</pre></code>`);
+  }
+
+  try {
+    history.pushState(null, '', 'b');
+  } catch (ex) {
+    logToConsole(`<code><pre>PushState Exception: ${ex.message}\n${ex.stack}</pre></code>`);
+  }
+});
+
